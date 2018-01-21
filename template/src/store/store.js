6cfd416ea5f5798f24c9cd9f03c1478446cbddc2
Kenneth Jakobsen <keja@users.noreply.github.com>
2018-01-21 13:39:37 +0100

    made jslint happy

--- template/src/store/store.js
+++ template/src/store/store.js
@@ -1,28 +1,30 @@
-import Vue from "vue"{{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
-import Vuex from "vuex"{{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
+/* eslint no-param-reassign: 0 */
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-export const store = new Vuex.Store({
-  //All data goes in to this object
+Vue.use(Vuex);
+export default new Vuex.Store({
+  // All data goes in to this object
   state: {
-    count: 0{{#if_eq lintConfig "airbnb"}},{{/if_eq}} 
+    count: 0,
   },
-  
-  //all changing of data happens in here (no async)
+
+  // all changing of data happens in here (no async)
   mutations: {
-    increment (state) {
-      state.count++{{#if_eq lintConfig "airbnb"}};{{/if_eq}} 
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
+    increment(state) {
+      state.count += 1;
+    },
   },
-  
-  //if async is needed in datachaingn make method here an call the mutation when it is done.
+
+  // if async is needed in datachaingn make method here an call the mutation when it is done.
   actions: {
-  
+
   },
-  
-  //if data needs to be returned in another format e.g.
+
+  // if data needs to be returned in another format e.g.
   getters: {
-    getCount (state){
-      return state.count{{#if_eq lintConfig "airbnb"}};{{/if_eq}} 
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
-}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
+    getCount(state) {
+      return state.count;
+    },
+  },
+});
