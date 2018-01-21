f04bbdb2a31b9f7c546d00f832836614126e6d43
Kenneth Jakobsen <keja@users.noreply.github.com>
2018-01-21 10:41:43 +0100

    added ; states depending on setup config

--- template/src/main.js
+++ template/src/main.js
@@ -7,10 +7,10 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
-import Vuex from "vuex"  
+import Vuex from "vuex"{{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex);
+Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
