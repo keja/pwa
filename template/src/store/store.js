f92bbd1c909f12c50e04282aa83389a50c157445
Kenneth Jakobsen <keja@users.noreply.github.com>
2018-01-21 13:17:41 +0100

    added store.js

---
+++ template/src/store/store.js
@@ -0,0 +1,28 @@
+import Vue from "vue"{{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
+import Vuex from "vuex"{{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
+
+export const store = new Vuex.Store({
+  //All data goes in to this object
+  state: {
+    count: 0{#if_eq lintConfig "airbnb"}},{{/if_eq}} 
+  },
+  
+  //all changing of data happens in here (no async)
+  mutations: {
+    increment (state) {
+      state.count++{{#if_eq lintConfig "airbnb"}};{{/if_eq}} 
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
+  },
+  
+  //if async is needed in datachaingn make method here an call the mutation when it is done.
+  actions: {
+  
+  },
+  
+  //if data needs to be returned in another format e.g.
+  getters: {
+    getCount (state){
+      return state.count{{#if_eq lintConfig "airbnb"}};{{/if_eq}} 
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
+  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}  
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}  
